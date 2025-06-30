7c1df1594e8b7f6a816bf12861ad84fca1c16087
mensahAmpomah <gideonmensah201@gmail.com>
2025-06-30 02:33:30 -0700

    Check space between sql commands

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -29,8 +29,8 @@ CREATE TABLE Customers(
 CREATE TABLE Orders(
     order_id INT PRIMARY KEY,
     customer_id INT ,
-    -- FOREIGN KEY(customer_id), REFERENCES Customers(customer_id),
     order_date DATE,
+        FOREIGN KEY (customer_id), REFERENCES Customers(customer_id),
 );
 
 CREATE TABLE Order_Details(
